212122a6732fb38c920630054bc3cc84d2f7b087
Axelkuul <akukuu@metropolia.fi>
2026-08-20 10:23:14 +0300

    tehtava tehty

---
+++ Tuntitehtava 1/Tehtävä 1 Tarina.py
@@ -0,0 +1,4 @@
+nimi = input("Mikä sun hahmon nimi on?: ")
+syntymävuosi = input("Minä vuonna hahmosi syntyi?:")
+adjektiivi = input("Sano yksi adjektiivi joka kuvailee hahmoasi: ")
+print(""+nimi+" oli rohkea ritari, hän syntyi vuonna "+syntymävuosi+" ja hän on erittäin "+adjektiivi+" " )
